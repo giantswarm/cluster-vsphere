71fd3b7cfd0a8a9b36ad8697c63ca0b56d5c122c
Puru <5674762+tuladhar@users.noreply.github.com>
2024-01-18 18:44:11 +0545

    Refactor

--- helm/cluster-vsphere/files/opt/teleport-node-role.sh
+++ helm/cluster-vsphere/files/opt/teleport-node-role.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# This script is executed by Teleport dynamic label configuration to assign the K8s role 
-# for the node (control-plane or worker)
-
-if systemctl is-active --quiet kubelet.service; then
+if systemctl is-active -q kubelet.service; then
     if [ -e "/etc/kubernetes/manifests/kube-apiserver.yaml" ]; then
         echo "control-plane"
     else
